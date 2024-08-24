10ad9a8eb457834fdd1dd586424e6a3511741432
heynar-max <heynar76@hotmail.com>
2024-08-23 21:26:18 -0500

    navbar pagesaint

--- src/IU/components/Navbar.jsx
+++ src/IU/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import {  Link, NavLink, useNavigate} from "react-router-dom"
 import '../../style/Navbar.css'
 import logo from '../../assets/saint.png'
+import { IoSearchOutline } from "react-icons/io5";
 
 
 export const Navbar = () => {
@@ -17,10 +18,16 @@ export const Navbar = () => {
     
     return (
         <nav className="nav_header">
+
+            {/* Logo  */}
+            <div>
                 <Link to="/">
-                    <img className="navbar_logo" src={logo}/>
+                        <img className="navbar_logo" src={logo}/>
                 </Link>
-                <div className="navbar-navigation">
+            </div>
+
+            {/* Center Menu  */}
+            <div className="center_menu">
                     <NavLink 
                         className={ ({isActive}) => `nav-item   ${ isActive ? 'active':'' }` } 
                         to="/bronze"
@@ -35,20 +42,60 @@ export const Navbar = () => {
                         Gold
                     </NavLink>
 
-                    <NavLink
-                    className={({isActive}) => `nav-item   ${ isActive ? 'active':'' }` }
-                    to="/search"
-                    >
-                        Search
-                    </NavLink>
+                    
 
+                </div>
+
+                {/* search, login, menu  */}
+                <div className="sidebar">
+                    <NavLink
+                        className={({isActive}) => `nav-item   ${ isActive ? 'active':'' }` }
+                        to="/search"
+                        >
+                            <IoSearchOutline />
+                    </NavLink>
                     <button
                         className="onlogout_botton"
                         onClick={ onLogout }
                     >
-                    Login
+                    menú
                     </button>
                 </div>
+                    
         </nav>
+        // <nav className="nav_header">
+        //         <Link to="/">
+        //             <img className="navbar_logo" src={logo}/>
+        //         </Link>
+        //         <div className="navbar-navigation">
+        //             <NavLink 
+        //                 className={ ({isActive}) => `nav-item   ${ isActive ? 'active':'' }` } 
+        //                 to="/bronze"
+        //             >
+        //                 Bronze
+        //             </NavLink>
+
+        //             <NavLink 
+        //                 className={ ({isActive}) => `nav-item   ${ isActive ? 'active':'' }` }
+        //                 to="/gold"
+        //             >
+        //                 Gold
+        //             </NavLink>
+
+        //             <NavLink
+        //             className={({isActive}) => `nav-item   ${ isActive ? 'active':'' }` }
+        //             to="/search"
+        //             >
+        //                 Search
+        //             </NavLink>
+
+        //             <button
+        //                 className="onlogout_botton"
+        //                 onClick={ onLogout }
+        //             >
+        //             Login
+        //             </button>
+        //         </div>
+        // </nav>
     )
 }
--- src/saintseiya/page/SaintPage.jsx
+++ src/saintseiya/page/SaintPage.jsx
@@ -1,12 +1,21 @@
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getSaintById } from "../helpers/getSaintById";
+import '../../style/SaintPage.css'
+import { FaChevronLeft } from "react-icons/fa";
 
 
 export const SaintPage = () => {
 
     const { id } = useParams();
+    // hook navegación
+    const navigate = useNavigate();
 
     const saint = getSaintById( id );
+
+    //  pagina anterior 
+    const onNavigateBack = () => {
+        navigate(-1);
+    }
 
     if ( !saint ) {
         // return <>404 - not found</> si quiere la pantalla 404
@@ -14,6 +23,144 @@ export const SaintPage = () => {
     }
 
     return (
-        <div>SaintPage</div>
+        <>
+
+
+        <div className="body">
+    <main>
+      <nav className="main-menu">
+        
+            <button
+                    onClick={onNavigateBack}>
+              
+              <FaChevronLeft  className="nav-icon"/>
+              <span className="nav-text">Regresar</span>
+            </button>
+      </nav>
+
+      <section className="content">
+        <div className="left-content">
+          <div className="activities">
+            <div className="user-info">
+            <div className="icon-container">
+            </div>
+            <h1>{saint.saint} de {saint.caballero}</h1>
+            <img src={ saint.image } />
+          </div>
+
+
+            <div className="activity-container">
+              <div className="image-container img-one">
+                <img src={ saint.image } alt={ saint.saint }/>
+                <div className="overlay">
+                  <h3>Saint</h3>
+                </div>
+              </div>
+
+              <div className="image-container img-two">
+                <img src={ saint.img } alt={ saint.saint } />
+                <div className="overlay">
+                  <h3>Cloth</h3>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+          <div className="left-bottom">
+            <div className="weekly-schedule">
+              <h1>Perfil</h1>
+              <div className="calendar">
+                <div className="day-and-activity activity-one">
+                  <div className="day">
+                    <h5>Nombre</h5>
+                  </div>
+                  <div className="activity">
+                    <h2>{saint.saint}</h2>
+                    
+                  </div>
+                </div>
+
+                <div className="day-and-activity activity-two">
+                  <div className="day">
+                    <h5>Caballero</h5>
+                  </div>
+                  <div className="activity">
+                    <h2>{saint.caballero}</h2>
+                    
+                  </div>
+                </div>
+
+                <div className="day-and-activity activity-three">
+                  <div className="day">
+                    <h5>Signo</h5>
+                  </div>
+                  <div className="activity">
+                    <h2>{saint.signo}</h2>
+                  </div>
+                </div>
+
+                  <div className="day-and-activity activity-four">
+                    <div className="day">
+                      <h5>Rango</h5>
+                      
+                    </div>
+                    <div className="activity">
+                      <h2>{saint.rango}</h2>
+                    </div>
+                  </div>
+              </div>
+            </div>
+
+            <div className="personal-bests">
+
+            <div className="day-and-activity activity-one">
+                  <div className="day">
+                    <h5>Armaduras</h5>
+                  </div>
+                  <div className="activity">
+                    <h2>{saint.armaduras}</h2>
+                    
+                  </div>
+                </div>
+
+                <div className="day-and-activity activity-two">
+                  <div className="day">
+                    <h5>Entrenamiento</h5>
+                  </div>
+                  <div className="activity">
+                    <h2>{saint.entrenamiento}</h2>
+                    
+                  </div>
+                </div>
+
+                <div className="day-and-activity activity-three">
+                  <div className="day">
+                    <h5>Maestro</h5>
+                  </div>
+                  <div className="activity">
+                    <h2>{saint.maestro}</h2>
+                  </div>
+                </div>
+
+                  <div className="day-and-activity activity-four">
+                    <div className="day">
+                      <h5>Nacimiento</h5>
+                      
+                    </div>
+                    <div className="activity">
+                      <h2>{saint.lugar_nacimiento}</h2>
+                    </div>
+                  </div>
+        
+            </div>
+          </div>
+        </div>
+
+      </section>
+    </main>
+  </div>
+
+        </>
     )
 }
